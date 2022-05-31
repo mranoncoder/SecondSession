51ff8538a6ee6c58fefa9ec42b41ed3e5d17d97c
Erfan Mougahi <99091183+Erphha@users.noreply.github.com>
2022-05-31 13:13:43 +0430

    Create SecondSession.sol
    Added a minor verification to the original.

---
+++ SecondSession.sol
@@ -0,0 +1,34 @@
+pragma solidity 0.8.7;
+
+contract SecondSession {
+
+string unVerifiedName = 'User not verified.';
+uint unverifiedAge = 0;
+
+   struct usersInitData{
+        string name;
+        uint age;        
+     }
+    
+    mapping(address=>usersInitData)private _userData;
+
+    function dataChangeHandler (string memory _name, uint _age) public{
+        if(_age <= 10  ){
+             _userData[msg.sender].name = unVerifiedName ;
+             _userData[msg.sender].age = unverifiedAge ;   
+        }else{
+            _userData[msg.sender].name = _name ;
+             _userData[msg.sender].age = _age ; 
+        }
+    }
+
+
+    function showName() public view returns(string memory){
+        return _userData[msg.sender].name;
+    }
+
+    function showAge() public view returns(uint){
+        return _userData[msg.sender].age;
+    }
+
+}
